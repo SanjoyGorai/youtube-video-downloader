06a61de702f55f24aeb6e4c0f942149ebbb897a0
Mr. Sanjoy <--add>
2024-08-02 09:10:42 +0530

    basic page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
+import { ThreeDot } from 'react-loading-indicators';
+import { Button } from './components/Button';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
@@ -11,16 +11,24 @@ function App() {
     setInputValue(event.target.value);
   };
 
- 
+  const url1 = 'https://www.youtube.com/watch?v=YWA-xbsJrVg'
+  const url2 = 'https://youtu.be/YWA-xbsJrVg?si=VB8MkWhxGEg7vtEX'
+
+  function extractVideoId(url) {
+    if (url.includes('youtu.be')) {
+      return url.split('/').pop().split('?')[0];
+    } else if (url.includes('youtube.com/watch')) {
+      return url.split('?v=')[1].split('&')[0];
+    }
+  }
 
   const handleButtonClick = (event) => {
     event.preventDefault()
     console.log(`Input value: ${inputValue}`);
     const videoUrl = inputValue;
-    const videoId = videoUrl.split('?')[0].split('/').pop();
-    console.log(videoId); // Output: YWA-xbsJrVg
+    const videoId = extractVideoId(videoUrl)
+    console.log(videoId);
     const url = `https://youtube-media-downloader.p.rapidapi.com/v2/video/details?videoId=${videoId}`;
-    // console.log("Url: ", url);
     const options = {
       method: 'GET',
       headers: {
@@ -29,24 +37,49 @@ function App() {
       }
     };
 
-    axios.get(url, options)
-      .then(r => console.log(r.data))
-      .catch(e => console.log(e));
+    // axios.get(url, options)
+    //   .then(r => console.log(r.data))
+    //   .catch(e => console.log(e));
+
+    const indi = document.getElementById('indi')
+    if (indi.id != null) {
+      console.log(indi.id);
+      indi.style.visibility = 'visible'
+    }
+
   };
 
 
   return (
     <>
-      <h1>Download video from Youtube 4k, 8k</h1>
-      <form action="">
+      <div className='border border-green-500 p-14 rounded'>
+        <h1 className='mb-4'>Download video from Youtube 4k, 8k</h1>
+        <form action="" className='mt-5 mb-4'>
 
-        <input type="text" value={inputValue} onChange={handleInputChange}
-          placeholder='Paste link here...' className='p-2.5 w-[480px]' />
+          <input type="text" value={inputValue} onChange={handleInputChange}
+            placeholder='Paste link here...' className='p-2.5 w-[480px] border
+           border-red-700 rounded-sm' />
 
-        <button type='submit' className='bg-pink-600'
-          onClick={handleButtonClick}>Start</button>
+          <button type='submit' className='bg-pink-600'
+            onClick={handleButtonClick}>Start</button>
 
-      </form>
+        </form>
+        <div>
+          <div className='flex flex-col items-start'>
+            <img src="/thumbnail.jpg" alt="thumb" />
+            <h5 className='font-bold'> How to Make a Website in 10 mins - Simple & Easy</h5>
+            <p>Duration: 00:11:22</p>
+          </div>
+          <div>
+            <Button />
+          </div>
+        </div>
+
+        <div className='mt-5 invisible' id='indi'>
+          <ThreeDot color="#32cd32" size="medium" text="" textColor="" />
+        </div>
+
+      </div>
     </>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Button = () => {
+    return (
+        <div className=''>
+            <button type='button' className='text-white bg-green-500 rounded'>Download</button>
+        </div>
+    )
+}
